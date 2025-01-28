5ff6754b0e7d1d10a8072804ff7f9180b590e455
Ruxanda Murtacaeva <murtacaevaruxanda@bk.ru>
2025-01-28 17:23:35 +0200

    Lesson 4.

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
+  const blogs = [
+    { title: 'Guguta finds hats', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'Alice finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'How to defeat monster', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404', { title: '404' });
 });
